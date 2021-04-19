2d593098e30b379ca6306a4f5e9a8be4bd971939
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-04-19 17:05:42 +0200

    Add files via upload

---
+++ section4/Coding exercises/src/PositiveNegativeZero.java
@@ -0,0 +1,9 @@
+    public static  vooid main (Striong [] args) {
+        cherckNumber(5);
+        checkNumber(-10);
+
+    }
+    public static void checkNumber(int number)
+        if(number > 0) {
+            sout
+            }
---
+++ section5/The while and do while/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+/*        int count = 6;
+        while (count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+        for (int i = 6; i != 6; i++) {
+            System.out.println("Count value is " + count);
+        }
+
+        count = 6;
+        do {
+            System.out.println("Count value was" + count);
+            count++;
+            if (count > 100) {
+                break;
+
+            }
+        } while (count != 6);*/
+
+        int number = 4;
+        int finishNumber = 20;
+
+        while (number <= finishNumber) {
+            number++;
+            if(!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println("Even number" + number);
+        }
+    }
+     public static boolean isEvenNumber(int number) {
+        if((number % 2) == 0){
+            return true;
+        }else {
+            return false;
+     }
+    }
+}
